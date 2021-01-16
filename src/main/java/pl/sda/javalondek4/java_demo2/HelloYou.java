ee5d4ea00e57d9d43f817bcba42d243224c12c71
@thiankiann <thiankiann@gmail.com>
2021-01-16 19:59:22 +0000

    [init] initial project commit

---
+++ src/main/java/pl/sda/javalondek4/java_demo2/HelloYou.java
@@ -0,0 +1,7 @@
+package pl.sda.javalondek4.java_demo2;
+
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("witajcie na zajesciach u Manka");
+    }
+}
