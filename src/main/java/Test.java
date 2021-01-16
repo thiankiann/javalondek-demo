91f9091d44f374f29697b9f432438cbc47137398
maniek <mpastuszka@gmail.com>
2021-01-16 11:31:11 +0100

    [init] - initial project commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+// never ever use default package!!!! You cannot import such class!!!
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Wrong hello world!!!");
+    }
+}
---
+++ src/main/java/pl/sda/javalondek4/java_demo/HelloYou.java
@@ -0,0 +1,8 @@
+// root package = groupId + artifactId
+package pl.sda.javalondek4.java_demo;
+
+public class HelloYou {
+    public static void main(String[] args) {
+        System.out.println("Witajcie na zajęciach u mańka:)");
+    }
+}
