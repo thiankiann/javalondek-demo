fef4f1960f1378d20d1044e1258034829a65f030
maniek <mpastuszka@gmail.com>
2021-01-17 09:50:04 +0100

    [refactor] - moving methods calls to separate one

--- src/main/java/pl/sda/javalondek4/java_demo/lambda/HelloLambda.java
+++ src/main/java/pl/sda/javalondek4/java_demo/lambda/HelloLambda.java
@@ -6,15 +6,30 @@ public class HelloLambda {
         ManiekRunner maniekRunner = new ManiekRunner();
         maniekRunner.walk();
         Runner runner = new ManiekRunner();
-        runner.go();
+        handleRunner(runner);
 //        runner.walk(); - doesn't compile
 
+        // define anonymous class
         Runner anonymousRunner = new Runner() {
             @Override
             public void go() {
                 System.out.println("Anonymous runner");
             }
         };
-        anonymousRunner.go();
+        handleRunner(anonymousRunner);
+
+        // call handleRunner user anonymous class
+        handleRunner(new Runner() {
+            @Override
+            public void go() {
+                System.out.println("inside method call");
+            }
+        });
+    }
+
+    public static void handleRunner(Runner anyRunner) {
+        System.out.println("calling handleRunner()");
+
+        anyRunner.go();
     }
 }
