a5bef938e17547772d3c18e339a83a890ece1402
@thiankiann <thiankiann@gmail.com>
2021-01-17 09:55:27 +0000

    [add] IIIcie  motatki s.3

--- src/main/java/pl/sda/javalondek4/java_demo2/lambda/HelloLambda.java
+++ src/main/java/pl/sda/javalondek4/java_demo2/lambda/HelloLambda.java
@@ -1,5 +1,5 @@
 package pl.sda.javalondek4.java_demo2.lambda;
-
+/*
 public class HelloLambda {
     public static void main(String[] args) {
 
@@ -22,6 +22,37 @@ public class HelloLambda {
     }
     public static void handleRunner(Runner anyRunner) {
         System.out.println("handleRunner()");
+        anyRunner.go();
+    }
+}
+*/
+public class HelloLambda {
+    public static void main(String[] args) {
+        ManiekRunner maniekRunner = new ManiekRunner();
+        maniekRunner.walk();
+        Runner runner = new ManiekRunner();
+        handleRunner(runner);
+//        runner.walk(); - doesn't compile
+        // define anonymous class
+        Runner anonymousRunner = new Runner() {
+            @Override
+            public void go() {
+                System.out.println("Anonymous runner");
+            }
+        };
+        handleRunner(anonymousRunner);
+        // call handleRunner user anonymous class
+        handleRunner(new Runner() {
+            @Override
+            public void go() {
+                System.out.println("inside method call");
+            }
+        });
+        // using lambda - super happy:)
+        handleRunner(() -> System.out.println("inside lambda call"));
+    }
+    public static void handleRunner(Runner anyRunner) {
+        System.out.println("calling handleRunner()");
         anyRunner.go();
     }
 }
