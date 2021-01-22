480d3358b1286a4ee7317879c8efc294d1666b69
@thiankiann <thiankiann@gmail.com>
2021-01-22 13:04:11 +0000

    [add]  added method crash() to class StaticDemo

--- src/main/java/pl/sda/javalondek4/java_demo2/lambda/StaticDemo.java
+++ src/main/java/pl/sda/javalondek4/java_demo2/lambda/StaticDemo.java
@@ -5,13 +5,17 @@ public class StaticDemo {
     public static void demo() {
         System.out.println("static demo");
     }
-
+    public  void crash() {
+        System.out.println("static demo");
+    }
     public static void main(String[] args) {
         demo();
         StaticDemo.demo();
 
         StaticDemo empty = null;
         empty.demo();
+
+        empty.crash();
     }
 
 }
