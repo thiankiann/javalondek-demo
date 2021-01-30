28c4d62cafa68abb38005776e75d2a6fc7adbae3
maniek <mpastuszka@gmail.com>
2021-01-30 10:58:14 +0100

    [change] - using var with type inference for Optional class

--- src/main/java/pl/sda/javalondek4/java_demo/optional/java7/ComputerUtils.java
+++ src/main/java/pl/sda/javalondek4/java_demo/optional/java7/ComputerUtils.java
@@ -1,5 +1,7 @@
 package pl.sda.javalondek4.java_demo.optional.java7;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class ComputerUtils {
@@ -39,6 +41,47 @@ public class ComputerUtils {
             .map(comp -> comp.getGraphicsCard())
             .map(graphicsCard -> graphicsCard.getModel())
             .orElse("no model");
+    }
+
+    public static String getGCModelWithJava8WithVar(Computer computer) {
+        /*
+        var in JavaScript
+        var name = "Mariusz";
+        var age = 18;
+        name = age;
+        age = "Ala"
+         */
+        var name = "Mariusz"; // String name = "Mariusz";
+//        var surname = null; var must have type!!!
+        var age = 5;
+        var ageL = 5L;
+        List<String> stringList = new ArrayList<String>(); // Java 6
+        List<String> stringList2 = new ArrayList<>(); // Java 7
+        var stringList3 = new ArrayList<>(); // better use that method below:) use new ArrayList<String>()
+//        stringList3.get(0);
+        var stringList4 = new ArrayList<String>(); // use this
+//        stringList4.get(0);
+//        String var; // var can be variable name :) var is context keyword!!!
+        var var = "name"; // don't do it
+
+//        Optional<Computer> maybeComputer = Optional.ofNullable(computer);
+//        Optional<GraphicsCard> maybeGraphicCard = maybeComputer.map(comp -> comp.getGraphicsCard());
+//        Optional<String> maybeModel = maybeGraphicCard.map(graphicsCard -> graphicsCard.getModel());
+//        return Optional.ofNullable(computer)
+//            .map(comp -> comp.getGraphicsCard())
+//            .map(graphicsCard -> graphicsCard.getModel())
+//            .orElse("no model");
+        var maybeComputer = Optional.ofNullable(computer);
+        var maybeGraphicCard = maybeComputer.map(comp -> comp.getGraphicsCard());
+        var maybeModel = maybeGraphicCard.map(graphicsCard -> graphicsCard.getModel());
+        // another approach
+        var maybeModelOfGC = Optional.ofNullable(computer)
+            .map(comp -> comp.getGraphicsCard())
+            .map(graphicsCard -> graphicsCard.getModel());
+
+        maybeModelOfGC.orElse("no model");
+
+        return maybeModel.orElse("no model");
     }
 
     public static void main(String[] args) {
